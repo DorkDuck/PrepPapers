e9501e750fafa7877fdf23bacea6bffd6c3c9921
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-12-07 20:34:17 +0530

    Dasdas

--- app.js
+++ app.js
@@ -12,22 +12,4 @@ app.use('/', home)
 
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
 module.exports = app;
