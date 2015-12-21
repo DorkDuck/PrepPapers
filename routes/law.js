1602b2c6f2b259ef8f62dedc1eaf16c6a95310dd
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-12-21 17:08:26 +0530

    fdfsdf

--- routes/law.js
+++ routes/law.js
@@ -1,9 +1,61 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
+var clatLinks = [
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com"
+]
 
+var ailetLinks = [
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com"
+]
 router.get('/clat', function(req, res, next) {
-  res.render('download_pages');
+  res.render('download_pages', {
+    title : "CLAT Papers",
+    years : [ 
+          "2015",
+          "2014",
+          "2013",
+          "2012",
+          "2011",
+          "2010",
+          "2009",
+          "2008"
+        ],
+    saal : 8,
+    links : clatLinks
+  })
 })
 
-module.exports = router;
+router.get('/ailet', function(req, res, next) {
+  res.render('download_pages', {
+    title : "AILET Papers",
+    years : [ 
+          "2015",
+          "2014",
+          "2013",
+          "2012",
+          "2011",
+          "2010",
+          "2009",
+          "2008"
+        ],
+    saal : 8,
+    links : ailetLinks
+  })
+})
+
+module.exports = router
--- routes/upsc.js
+++ routes/upsc.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 
 router.get('/', function(req, res, next) {
-  res.render('upsc_subjects');
+  res.render('upsc_subjects', {
+  	years: 7,
+  	year: [
+  		"2015",
+          "2014",
+          "2013",
+          "2012",
+          "2011",
+          "2010",
+          "2009"
+  	],
+  	links: [
+  		"http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com",
+      "http://www.google.com"
+  	]
+  })
 })
 
 
-module.exports = router;
+module.exports = router
