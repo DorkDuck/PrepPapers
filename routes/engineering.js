8f21090dcbca54887dd989a0880f87bb8b494dbc
Sarthak Munshi <sarthakmunshi@gmail.com>
2016-01-05 22:33:25 +0530

    JEE and Management Links Added

--- routes/engineering.js
+++ routes/engineering.js
@@ -2,15 +2,15 @@ var express = require('express')
 var router = express.Router()
 
 var jeemLinks = [
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com"
+      "http://www.vidyalankar.org/file/JEEMain/JEEMAIN-2015_Question_Paper_Solution.pdf",
+      "http://www.vidyalankar.org/file/JEE-MAIN-2014-Question-Paper-and-Solution.pdf",
+      "http://www.vidyalankar.org/file/iit_jee/JEE-MAIN-2013-Question-Paper-and-Solution.pdf"
 ]
 
 var jeeaLinks = [
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com"
+      "http://www.vidyalankar.org/jee-advanced-2015-solution-answerkey.aspx",
+      "http://admission.aglasem.com/iit-jee-advanced-2014-question-paper-answers/",
+      "http://admission.aglasem.com/iit-jee-advanced-2013-question-paper-answers/"
 ]
 
 router.get('/jee-main', function(req, res, next) {
--- routes/management.js
+++ routes/management.js
@@ -2,34 +2,33 @@ var express = require('express')
 var router = express.Router()
 
 var catLinks = [
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com"
+      "http://mba.hitbullseye.com/pdf/cat-2015-analysis.pdf",
+      "https://drive.google.com/file/d/0B5SdM6PJadyEMnB6NHNveFBndk0/edit",
+      "http://www.indiaeducation.net/management/CAT/download/CAT-Sample-Paper-1.pdf",
+      "https://drive.google.com/file/d/0B45Zt7UZ_h2ocFNhR0FyakxabXM/edit",
+      "http://admission.aglasem.com/cat-2011-previous-year-question-paper-answers/",
+      "http://www.mywbut.com/cat/Papers/CAT%202010%20Paper%20with%20Answer%20Keys.pdf",
+      "http://www.mywbut.com/cat/Papers/CAT%202009%20Paper%20with%20Answer%20Keys.pdf",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PSUltR1AyUFdRVzQ/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PTmJ3SWZCTUVTNWs/edit",
+      "https://drive.google.com/file/d/0B1MNt6pxsDvIdDNVVWJoMEZJb3c/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PTEZOanhnNlAwYWc/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PQ082ejZTSHRBQjg/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PU3g2X0lTWnBIb0k/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PbVNPRTVmeW5oNkU/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PMEduZVQ1VG9vZVU/edit",
+      "https://drive.google.com/file/d/0B7KCO87PWJ1PSTRpbE5wcDA0X3M/edit"
 ]
 
 var xatLinks = [
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com",
-      "http://www.google.com"
+      "http://admission.aglasem.com/xat-2015-question-paper-with-solutions/",
+      "http://admission.aglasem.com/xat-2014-question-paper-with-solutions/",
+      "http://admission.aglasem.com/xat-2013-question-paper-with-solutions/",
+      "http://admission.aglasem.com/xat-2012-question-paper-with-solutions/",
+      "http://admission.aglasem.com/xat-2011-question-paper-with-solutions/",
+      "http://admission.aglasem.com/xat-2010-question-paper-with-solutions/",
+      "http://testfunda.com/DownloadHandler.AssessDwd?strFilePath=/Content/PastTestPapers/XAT/XAT_2009_Question_Paper_and_Ans_Key.pdf",
+      "http://testfunda.com/DownloadHandler.AssessDwd?strFilePath=/Content/PastTestPapers/XAT/XAT_2008_Question_Paper_and_Ans_Key.pdf"
 ]
 router.get('/cat', function(req, res, next) {
   res.render('download_pages', {
@@ -50,10 +49,9 @@ router.get('/cat', function(req, res, next) {
           "2003",
           "2002",
           "2001",
-          "2000",
-          "1999"
+          "2000"
         ],
-    saal : 17,
+    saal : 16,
     links : catLinks
   })
 })
