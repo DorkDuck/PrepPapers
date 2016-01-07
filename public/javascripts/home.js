e177e88fb0f949a562732970a6335f895b58e5a1
Sarthak Munshi <sarthakmunshi@gmail.com>
2016-01-07 15:24:05 +0530

    socket.io url parameters set

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,5 @@
 
- var socket = io.connect("http://preppapers.com/");
+ var socket = io.connect(window.location.href);
     socket.on('connect', function () {
       socket.on('current_users', function(data) {
         $('.son').html(data.value)
